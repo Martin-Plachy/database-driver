05cf946f58dc34c2814486e4ac2d5df62bf2dec4
Martin Plachý <martin.plachy86@gmail.com>
2023-03-30 15:14:36 +0200

    The index.php file added. The file contains a declaration of input variables and class DatabaseDriver calling.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'config.php';
+require_once 'databasedriver.php';
+
+$dbName = "database0Name";
+$tableName = "table0Name";
+
+$rowTableInputArray = [
+    'property0' => 0,
+    'property1' => 1,
+    'property2' => 2,
+    'property3' => 3,
+    'property4' => 4,
+];
+
+$rowTableInputArrayKeys = array_keys($rowTableInputArray);
+
+$primary_key_index = 0;
+
+DatabaseDriver::connectDatabaseServer($dsn, $username, $password, $dbName);
+DatabaseDriver::createTable($tableName, $rowTableInputArray, $primary_key_index);
+DatabaseDriver::insertOneRowInTable($tableName, $rowTableInputArray);
+DatabaseDriver::disconnectDatabaseServer();
