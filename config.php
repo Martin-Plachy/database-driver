cfd63fe14f209f7440d52d2dcec1657fd3948118
Martin Plachý <martin.plachy86@gmail.com>
2023-04-06 12:24:41 +0200

    Config variables changed to constants.

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dsn = "mysql:host=$servername";
+define("DB_SERVERNAME","127.0.0.1");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_DSN","mysql:host=" . DB_SERVERNAME);
